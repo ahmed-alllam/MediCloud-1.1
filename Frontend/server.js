1d97de42c49f2232a6875bc68cec09fdf8faf827
Ahmed Allam <ahmedallam@Ahmeds-MacBook-Air.local>
2022-08-10 15:13:11 +0200

    Added SSL security

--- Frontend/server.js
+++ Frontend/server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const path = require('path');
 app = express();
-app.use(express.static(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 443;
 
 app.enable('trust proxy')
 app.use((req, res, next) => {
+    console.log("is secure? " + req.secure)
     req.secure ? next() : res.redirect('https://' + req.headers.host + req.url)
 })
+app.use(express.static(path.join(__dirname, 'dist')));
+
+const port = process.env.PORT || 443;
+
 
 app.listen(port);
