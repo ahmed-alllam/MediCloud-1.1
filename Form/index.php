313de8330c83dd9b9503847f1805671ba7b0cddc
Ahmed Allam <ahmed39652003@gmail.com>
2022-10-11 16:38:04 +0200

    Added forms

--- Form/index.php
+++ Form/index.php
@@ -1,1 +1,11 @@
-<?php include_once("home.html"); ?>
+<?php 
+
+if ($_SERVER['HTTP_HOST'] != 'localhost' and $_SERVER['HTTP_X_FORWARDED_PROTO'] != "https") {
+    $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    header('HTTP/1.1 301 Moved Permanently');
+    header('Location: ' . $location);
+    exit;
+}
+
+
+include_once("home.html"); ?>
